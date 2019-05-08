fb360908f8b53ec1b14bcc2da0e89cde7fe1b4ec
Hardspyne <79159707922@yandex.ru>
2019-05-08 15:18:08 +0300

    create project

---
+++ src/main/java/com/zubarev/websiteavailabilitychecker/WebsiteAvailabilityScheduledCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.zubarev.websiteavailabilitychecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteAvailabilityScheduledCheckerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsiteAvailabilityScheduledCheckerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zubarev/websiteavailabilitychecker/WebsiteAvailabilityScheduledCheckerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zubarev.websiteavailabilitychecker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebsiteAvailabilityScheduledCheckerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
