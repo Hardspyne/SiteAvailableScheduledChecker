1b1202e0d060df0b69c83122f7e2c4428880c51c
Hardspyne <79159707922@yandex.ru>
2019-05-08 18:57:55 +0300

    update util

--- src/main/java/com/zubarev/websiteavailabilitychecker/utils/HttpUrlConnectionUtil.java
+++ src/main/java/com/zubarev/websiteavailabilitychecker/utils/HttpUrlConnectionUtil.java
@@ -1,17 +1,27 @@
 package com.zubarev.websiteavailabilitychecker.utils;
 
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
 import java.io.IOException;
-import java.net.HttpURLConnection;
+import java.net.InetAddress;
 import java.net.URL;
+
 
 public class HttpUrlConnectionUtil {
 
     public static int getStatusCode(String site) throws IOException {
-        URL url = new URL(site);
-        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-        connection.setRequestMethod("GET");
-        connection.connect();
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        HttpResponse resp = httpClient.execute(new HttpGet(site));
+        return resp.getStatusLine().getStatusCode();
+    }
 
-        return connection.getResponseCode();
+    public static boolean isReachable(String site) throws IOException {
+        InetAddress inetAddress =  InetAddress.getByName(new URL(site).getHost());
+        return inetAddress.isReachable(10000);
     }
 }
