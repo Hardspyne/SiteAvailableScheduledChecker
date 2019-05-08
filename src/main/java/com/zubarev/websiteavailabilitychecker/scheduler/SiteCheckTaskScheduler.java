3713dadee62b97d9b0becd46eca120fc661ebd06
Hardspyne <79159707922@yandex.ru>
2019-05-08 18:17:58 +0300

    update TaskScheduler

--- src/main/java/com/zubarev/websiteavailabilitychecker/scheduler/SiteCheckTaskScheduler.java
+++ src/main/java/com/zubarev/websiteavailabilitychecker/scheduler/SiteCheckTaskScheduler.java
@@ -1,21 +1,39 @@
 package com.zubarev.websiteavailabilitychecker.scheduler;
 
 import com.zubarev.websiteavailabilitychecker.model.SiteCheckScheduledTask;
+import com.zubarev.websiteavailabilitychecker.utils.HttpUrlConnectionUtil;
+import com.zubarev.websiteavailabilitychecker.utils.MailSenderUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class SiteCheckTaskScheduler {
 
-    public int checkSiteStatus(SiteCheckScheduledTask task) throws IOException {
-        URL url = new URL("http://example.com");
-        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-        connection.setRequestMethod("GET");
-        connection.connect();
+    @Autowired
+    private MailSenderUtil mailSenderUtil;
 
-        return connection.getResponseCode();
+    public int checkSiteStatus(SiteCheckScheduledTask task) throws IOException {
+        int statusCode = HttpUrlConnectionUtil.getStatusCode(task.getSite());
+        new Thread(() -> {
+            while (true) {
+                try {
+                    TimeUnit.MINUTES.sleep(task.getRefreshTime());
+
+                    int currentStatusCode = HttpUrlConnectionUtil.getStatusCode(task.getSite());
+
+                    mailSenderUtil.sendMessage(task.getEmail(),task.getSite(),currentStatusCode);
+                    if (statusCode != currentStatusCode) {
+                        break;
+                    }
+                } catch (InterruptedException | IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        return statusCode;
     }
 }
