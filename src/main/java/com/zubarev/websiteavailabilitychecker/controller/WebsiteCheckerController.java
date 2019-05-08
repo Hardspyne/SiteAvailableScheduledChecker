ca98389a364b7e62e6eacb8e3607a3501a232a76
Hardspyne <79159707922@yandex.ru>
2019-05-08 16:21:38 +0300

    add controller

--- src/main/java/com/zubarev/websiteavailabilitychecker/controller/WebsiteCheckerController.java
+++ src/main/java/com/zubarev/websiteavailabilitychecker/controller/WebsiteCheckerController.java
@@ -1,8 +1,34 @@
 package com.zubarev.websiteavailabilitychecker.controller;
 
+import com.zubarev.websiteavailabilitychecker.model.WebSiteCheckScheduledTask;
+import com.zubarev.websiteavailabilitychecker.scheduler.WebSiteCheckTaskScheduler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.io.IOException;
 
 @Controller
 public class WebsiteCheckerController {
+
+    @Autowired
+    WebSiteCheckTaskScheduler webSiteCheckTaskScheduler;
+
+    @GetMapping("/")
+    public String createScheduledTask(Model model) {
+        WebSiteCheckScheduledTask webSiteCheckScheduledTask = new WebSiteCheckScheduledTask();
+        model.addAttribute("task", webSiteCheckScheduledTask);
+        return "task-schedule";
+    }
+
+    @PostMapping("/start-task")
+    public String startScheduledTask(@ModelAttribute("task") WebSiteCheckScheduledTask webSiteCheckScheduledTask, Model model) throws IOException {
+        model.addAttribute("status", webSiteCheckTaskScheduler.checkSiteStatus());
+        return "result";
+    }
+
 
 }
