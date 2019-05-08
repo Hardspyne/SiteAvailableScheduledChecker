4b79f61c96717479cbc4cc03a43360ff58e41f0e
Hardspyne <79159707922@yandex.ru>
2019-05-08 15:56:02 +0300

    add task

---
+++ src/main/java/com/zubarev/websiteavailabilitychecker/model/ScheduledTask.java
@@ -0,0 +1,41 @@
+package com.zubarev.websiteavailabilitychecker.model;
+
+public class ScheduledTask {
+    private String site;
+    private int refreshTime;
+    private String email;
+
+    public ScheduledTask(String site, int refreshTime, String email) {
+        this.site = site;
+        this.refreshTime = refreshTime;
+        this.email = email;
+    }
+
+    public ScheduledTask() {
+    }
+
+
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+
+    public int getRefreshTime() {
+        return refreshTime;
+    }
+
+    public void setRefreshTime(int refreshTime) {
+        this.refreshTime = refreshTime;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
